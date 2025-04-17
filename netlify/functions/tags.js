d91daff8c3dab80231c30ac63bf9c855d30c0ff2
netlifyjoe <joe.keyes@netlify.com>
2025-04-17 12:25:09 -0400

    Test

--- netlify/functions/tags.js
+++ netlify/functions/tags.js
@@ -1,12 +1,20 @@
 export default async (req, context) => {
-    const response = await fetch('http://localhost:11434/api/tags', {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    })
+    try {
+        const response = await fetch('http://localhost:11434/api/tags', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
 
-    return await response;
+        return await response;
+    } catch (ex) {
+        console.error(ex);
+        return new Response(JSON.stringify({ error: ex }), {
+            headers: { 'Content-Type': 'application/json' },
+            status: 500
+        });
+    }
 };
 
 export const config = {
